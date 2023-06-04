198d44b538006f192011309d190bdba155ba5e4f
Usman Abdur Rehman <saidskit@outlook.com>
2023-06-04 22:57:09 +0500

    Next 13 Route Groups Tutorial

---
+++ index.js
@@ -0,0 +1,13 @@
+/**
+ * Route Groups dont add anything to the route
+ * (doctors)/page.js
+ *
+ * Cant have multiple pages for the same route because now can do that.
+ * (doctors)/page.js (patients)/page.js
+ *
+ * Can have root layout for some pages
+ * (doctors) pages under
+ *
+ * Can have multiple root layouts
+ * (doctors)    (patients)
+ */
