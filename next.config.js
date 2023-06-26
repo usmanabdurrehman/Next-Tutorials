fb6ebf131e5813b947d440d390b312be799733ce
Usman Abdur Rehman <saidskit@outlook.com>
2023-06-26 14:12:21 +0500

    Added base changes for main branch

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  reactStrictMode: false,
   swcMinify: true,
-}
+  experimental: {
+    appDir: true,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
